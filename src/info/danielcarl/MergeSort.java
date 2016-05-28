0f191cbef44f3594b30265f191845d3618c1ba3a
Daniel Carl Jones <dcjones1@sheffield.ac.uk>
2016-05-28 18:22:29 +0100

    Implement Merge Sort.

--- src/info/danielcarl/MergeSort.java
+++ src/info/danielcarl/MergeSort.java
@@ -6,31 +6,48 @@ public class MergeSort extends Sort {
 
     System.out.println("Unsorted Array: " + formatArray(numbers));
 
-//    sort(numbers);
-//    System.out.println("Sorted Array: " + formatArray(numbers));
+    mergeSort(numbers, 0, numbers.length - 1);
+    System.out.println("Sorted Array: " + formatArray(numbers));
   }
 
-  public static void merge(int[] array, int startPos, int midPos, int endPos) {
-    int leftLength = midPos - startPos + 1;
-    int rightLength = endPos - startPos + 1;
-    int[] leftArray = new int[leftLength], rightArray = new int[rightLength];
+  private static void merge(int[] array, int startPos, int midPos, int endPos) {
+    int[] leftArray = new int[midPos - startPos + 1];
+    int[] rightArray = new int[endPos - midPos];
 
     // Copy array data across. (Oh no, this algorithm isn't in-place!)
-    for (int x = 0; x < leftLength; x++) {
+    for (int x = 0; x < leftArray.length; x++) {
       leftArray[x] = array[startPos + x];
     }
-    for (int x = 0; x < rightLength; x++) {
+    for (int x = 0; x < rightArray.length; x++) {
       rightArray[x] = array[midPos + x + 1];
     }
 
     // Begin merge!
-    int i = 0, j = 0;
+    int lPointer = 0, rPointer = 0;
     for (int k = startPos; k <= endPos; k++) {
-      if (leftArray[i] <= rightArray[j]) {
-        array[k] = leftArray[i++];
+      if (lPointer >= leftArray.length) {
+        // If we've exhausted the left array...
+        array[k] = rightArray[rPointer++];
+      } else if (rPointer >= rightArray.length) {
+        // If we've exhausted the right array...
+        array[k] = leftArray[lPointer++];
       } else {
-        array[k] = rightArray[j++];
+        // Still some elements left in leftArray and rightArray!
+        if (leftArray[lPointer] <= rightArray[rPointer]) {
+          array[k] = leftArray[lPointer++];
+        } else {
+          array[k] = rightArray[rPointer++];
+        }
       }
+    }
+  }
+
+  public static void mergeSort(int[] array, int startPos, int endPos) {
+    if (startPos < endPos) {
+      int midPos = (startPos + endPos) / 2;
+      mergeSort(array, startPos, midPos);
+      mergeSort(array, midPos + 1, endPos);
+      merge(array, startPos, midPos, endPos);
     }
   }
 }
