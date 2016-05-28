36efc52a560d2f86b13b538767a86f6c948999d4
Daniel Carl Jones <dcjones1@sheffield.ac.uk>
2016-05-28 15:14:15 +0100

    Implement Insertion Sort.

--- src/info/danielcarl/InsertionSort.java
+++ src/info/danielcarl/InsertionSort.java
@@ -2,6 +2,34 @@ package info.danielcarl;
 
 class InsertionSort {
   public static void main(String[] args) {
-    System.out.println("Project.");
+    int[] numbers = { 5, 2, 4, 6, 1, 3 };
+
+    System.out.println("Unsorted Array: " + formatArray(numbers));
+
+    sort(numbers);
+    System.out.println("Sorted Array: " + formatArray(numbers));
+  }
+
+  public static void sort(int[] numArray) {
+    int key, i;
+    for (int j = 1; j < numArray.length; j++) {
+      // Insert each key into correct position...
+      key = numArray[j];
+      i = j - 1;
+      while (i >= 0 && numArray[i] > key) {
+        // Until key is in correct position, move each number along.
+        numArray[i + 1] = numArray[i];
+        i--;
+      }
+      numArray[i + 1] = key; // Place in final position.
+    }
+  }
+
+  private static String formatArray(int[] numArray) {
+    String str = "";
+    for (int number : numArray) {
+      str += number + "; ";
+    }
+    return str;
   }
 }
